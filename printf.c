417cf58d4af7136cf64d54265d8f264f6abc3afa
Hordunlarmy <Hordunlarmy@gmail.com>
2023-04-25 13:16:28 +0100

    Refactor printf.c to simplify the code and fix a bug where non-format characters were not counted in the return value

--- printf.c
+++ printf.c
@@ -19,7 +19,10 @@ int _printf(const char *format, ...)
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
-			r_value += put_c(format[i]);
+		{
+			put_c(format[i]);
+			r_value++;
+		}
 
 		else
 		{
@@ -27,18 +30,14 @@ int _printf(const char *format, ...)
 				return (-1);
 			if (format[i + 1] == '%' || format[i + 1] == 'c' ||
 					format[i + 1] == 's' || format[i + 1] == 'd' || format[i + 1] == 'i')
-			{
 				sr_value = spec_handle1(ap, format[i + 1]);
-				if (sr_value == -1)
-					return (-1);
-			}
-			else if (format[i + 1] == 'b' || format[i + 1] == 'u' ||
+
+			if (format[i + 1] == 'b' || format[i + 1] == 'u' ||
 					format[i + 1] == 'o' || format[i + 1] == 'x' || format[i + 1] == 'X')
-			{
 				sr_value = spec_handle2(ap, format[i + 1]);
-				if (sr_value == -1)
-					return (-1);
-			}
+
+			if (sr_value == -1)
+				return (-1);
 			r_value += sr_value;
 			i++;
 		}
