09c7adffadb2de85ae9a99875fc6b79c3d5b2438
Chidimmakalu <Chidimmakalu2000@gmail.com>
2023-04-25 07:48:24 -0700

    refactored special specifier

--- puts.c
+++ puts.c
@@ -30,22 +30,25 @@ int put_s(char *string)
 int put_S(char *string)
 {
 	int i, r_value = 0;
+	int c;
 
 	if (string == NULL)
+		string = "(null)";
+	for (i = 0; *(string + i) != '\0'; i++)
 	{
-		return (put_s("(null)"));
-	}
-	for (i = 0; string[i] != '\0'; i++)
-	{
-		if (string[i] >= 32 && string[i] < 127)
+		if (*(string + i) < 32 || *(string + i) >= 127)
 		{
-			r_value += put_c(string[i]);
+			put_c('\\');
+			put_c('x');
+			r_value = r_value + 2;
+			c = *(string + i);
+			if (c < 16)
+				r_value += put_c('0');
+
+			r_value = r_value + put_X(cast);
 		}
 		else
-		{
-			r_value += put_s("\\x");
-			r_value += put_x((unsigned int)string[i]);
-		}
+			r_value += *(string + i);
 	}
 	return (r_value);
 }
