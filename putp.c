2677c51b6fddf754ab3eeeafc54ee51207e7dc7f
Chidimmakalu <Chidimmakalu2000@gmail.com>
2023-04-25 04:00:39 -0700

    changes reverted

--- putp.c
+++ putp.c
@@ -8,7 +8,7 @@
 int put_p(void *ptr)
 {
 	char buffer[1024];
-	size_t i;
+	size_t i, r;
 	int r_value;
 
 	r_value = snprintf(buffer, sizeof(buffer), "%p", ptr);
@@ -18,7 +18,13 @@ int put_p(void *ptr)
 	}
 
 	for (i = 0; i < (size_t)r_value; i++)
-		putchar(buffer[i]);
+	{
+		r = putchar(buffer[i]);
+		if (r < 0)
+		{
+			return (-1);
+		}
+	}
 
 	return (r_value);
 }
