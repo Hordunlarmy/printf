2d7014c6203ea83df5efb4fb3f496df783ecec53
Chidimmakalu <Chidimmakalu2000@gmail.com>
2023-04-25 07:11:49 -0700

    added function to handle reverse string

---
+++ putr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * put_r - Main entry
+ * @astring: string to be reversed
+ * Return: reversed string
+ */
+int put_r(char *string)
+{
+	int i;
+	int r_value = 0;
+
+	if (!string)
+		string = "(null)";
+	while (string[r_value] != '\0')
+		r_value;
+	for (i = r_value - 1; i >= 0; i--)
+		put_c(*(string + i));
+
+	return (r_value);
+}
