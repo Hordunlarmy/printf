0aa2705f0d1be7b50e203ea729e78237ed7a477c
Hordunlarmy <Hordunlarmy@gmail.com>
2023-04-25 12:19:16 +0100

    Refactor printf.c to use two separate functions for handling specifiers based on their type

--- specifiers.c
+++ specifiers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * spec_handle - Entry point
+ * spec_handle1 - Entry point
  * @specifier: specifier
  * @ap: arguement parameter
  * Return: success
  */
-int spec_handle(va_list ap, char specifier)
+int spec_handle1(va_list ap, char specifier)
 {
 	int r_value = 0;
 
@@ -21,21 +21,6 @@ int spec_handle(va_list ap, char specifier)
 		case 's':
 			r_value += put_s(va_arg(ap, char *));
 			break;
-		case 'b':
-			r_value += put_b(va_arg(ap, int));
-			break;
-			case 'u':
-			r_value += put_u(va_arg(ap, unsigned int));
-			break;
-			case 'o':
-			r_value += put_o(va_arg(ap, unsigned int));
-			break;
-			case 'x':
-			r_value += put_x(va_arg(ap, unsigned int));
-			break;
-			case 'X':
-			r_value += put_X(va_arg(ap, unsigned int));
-			break;
 		case 'd':
 		case 'i':
 			r_value += put_d(va_arg(ap, int));
@@ -47,4 +32,41 @@ int spec_handle(va_list ap, char specifier)
 				break;
 	}
 	return (r_value);
+}
+
+/**
+ * spec_handle2 - Entry point
+ * @specifier: specifier
+ * @ap: arguement parameter
+ * Return: success
+ */
+
+int spec_handle2(va_list ap, char specifier)
+{
+	int r_value = 0;
+
+	switch (specifier)
+	{
+	case 'b':
+		r_value += put_b(va_arg(ap, int));
+		break;
+	case 'u':
+		r_value += put_u(va_arg(ap, unsigned int));
+		break;
+	case 'o':
+		r_value += put_o(va_arg(ap, unsigned int));
+		break;
+	case 'x':
+		r_value += put_x(va_arg(ap, unsigned int));
+		break;
+	case 'X':
+		r_value += put_X(va_arg(ap, unsigned int));
+		break;
+		default:
+		put_c('%');
+		put_c(specifier);
+		r_value += 2;
+		break;
+	}
+return (r_value);
 }
