c8888fac1eaf154069bb06e497931ebc7f02eca6
Hordunlarmy <Hordunlarmy@gmail.com>
2023-04-25 17:24:06 +0100

    Refactor put_S to improve readability and maintainability

--- puts.c
+++ puts.c
@@ -30,25 +30,26 @@ int put_s(char *string)
 int put_S(char *string)
 {
 	int i, r_value = 0;
-	int c;
 
 	if (string == NULL)
 		string = "(null)";
+
 	for (i = 0; *(string + i) != '\0'; i++)
 	{
-		if (*(string + i) < 32 || *(string + i) >= 127)
+		if (((string[i]) > 0 && *(string + i) < 32)
+			|| *(string + i) >= 127)
 		{
 			put_c('\\');
 			put_c('x');
-			r_value = r_value + 2;
-			c = *(string + i);
-			if (c < 16)
+			r_value += 2;
+
+			if (*(string + i) < 16)
 				r_value += put_c('0');
 
-			r_value = r_value + put_X(c);
+			r_value += put_X(*(string + i));
 		}
 		else
-			r_value += *(string + i);
+			r_value += put_c(*(string + i));
 	}
 	return (r_value);
 }
